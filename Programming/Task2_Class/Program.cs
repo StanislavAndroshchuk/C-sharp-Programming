740f139c5ea323d5c68cbb991eae5c148fe6c333
StanislavAndroshchuk <newmegodvenm@gmail.com>
2023-05-04 10:45:33 +0300

    changes

--- Programming/Task2_Class/Program.cs
+++ Programming/Task2_Class/Program.cs
@@ -50,19 +50,12 @@ namespace Task2_Class
                 Console.WriteLine("2.View list by id");
                 Console.WriteLine("3.Sort list");
                 Console.WriteLine("4.Search in list");
-                if (currentUser.Role == Roles.Admin)
-                {
-                    Console.WriteLine("5.Delete element in list");
+                Console.WriteLine("5.Delete element in list");
                     Console.WriteLine("6.Add element in list");
                     Console.WriteLine("7.Edit element in list");
                     Console.WriteLine("8.Sign out");
-                }
-                else
-                {
-                    Console.WriteLine("5.Sign out");
-                }
-                
-                Console.Write("? : ");
+
+                    Console.Write("? : ");
                 option = Console.ReadLine()!;
                 Console.Clear();
                 try
@@ -108,74 +101,66 @@ namespace Task2_Class
                             Console.WriteLine(element);
                         }
                     }
-
-                    else if (currentUser.Role == Roles.Admin)
+                    if (option == "5")
                     {
-                        if (option == "5")
-                        {
-                            Console.Write("Delete by id: ");
-                            string line = Console.ReadLine()!;
-                            int id = Validation.ValidPositiveInt(line);
-                            userProxy.Delete(id);
-                            collection.Rewrite(pathToCollection);
-                        }
-                        else if (option == "6")
-                        {
-                            Console.WriteLine("Input new element : ");
-                            Order item = new Order();
-                            item.ToWrite(collection.Count);
-                            userProxy.Create(item);
-                            collection.Rewrite(pathToCollection);
-                        }
-                        else if (option == "7")
-                        {
-                            var validFields = collection.GetValidFields();
-                            Console.Write("Id element to edit: ");
-                            string id = Console.ReadLine()!;
-                            int validId = Validation.ValidPositiveInt(id);
-                            Console.Write("Attribute of element to edit: ");
-                            string attribute = Console.ReadLine()!;
-                            Console.Write("Value to change: ");
-                            string value = Console.ReadLine()!;
-                            if (validFields.ContainsKey(attribute))
-                            {
-                                try
-                                {
-                                    if (attribute == "ShippedDate")
-                                    {
-                                        var validValue = validFields[attribute].DynamicInvoke(value,
-                                            collection.Collection[validId].GetType().GetProperty("OrderDate"));
-                                        userProxy.Edit(validId, attribute, validValue!);
-                                    }
-                                    else
-                                    {
-                                        var validValue = validFields[attribute].DynamicInvoke(value);
-                                        userProxy.Edit(validId, attribute, validValue!);
-                                    }
-                                    
-                                    collection.Rewrite(pathToCollection);
-                                }
-                                catch (ArgumentOutOfRangeException)
-                                {
-                                    Console.WriteLine("Incorrect id input!");
-                                }
-                                catch (Exception er)
-                                {
-                                    Console.WriteLine(er.InnerException?.Message);
-                                }
-                            }
-                            else
-                            {
-                                Console.WriteLine("Incorrect attribute name of element!");
-                            }
-                            
-                        }
-                        else if (option == "8")
-                        {
-                            goto Logging;
-                        }
+                        Console.Write("Delete by id: ");
+                        string line = Console.ReadLine()!;
+                        int id = Validation.ValidPositiveInt(line);
+                        userProxy.Delete(id);
+                        collection.Rewrite(pathToCollection);
                     }
-                    else if (option == "5" && currentUser.Role == Roles.Customer)
+                    else if (option == "6")
+                    {
+                        Console.WriteLine("Input new element : ");
+                        Order item = new Order();
+                        item.ToWrite(collection.Count);
+                        userProxy.Create(item);
+                        collection.Rewrite(pathToCollection);
+                    }
+                    else if (option == "7")
+                    {
+                        var validFields = collection.GetValidFields();
+                        Console.Write("Id element to edit: ");
+                        string id = Console.ReadLine()!;
+                        int validId = Validation.ValidPositiveInt(id);
+                        Console.Write("Attribute of element to edit: ");
+                        string attribute = Console.ReadLine()!;
+                        Console.Write("Value to change: ");
+                        string value = Console.ReadLine()!;
+                        if (validFields.ContainsKey(attribute))
+                        {
+                            try
+                            {
+                                if (attribute == "ShippedDate")
+                                {
+                                    var validValue = validFields[attribute].DynamicInvoke(value,
+                                        collection.Collection[validId].GetType().GetProperty("OrderDate"));
+                                    userProxy.Edit(validId, attribute, validValue!);
+                                }
+                                else
+                                {
+                                    var validValue = validFields[attribute].DynamicInvoke(value);
+                                    userProxy.Edit(validId, attribute, validValue!);
+                                }
+                                
+                                collection.Rewrite(pathToCollection);
+                            }
+                            catch (ArgumentOutOfRangeException)
+                            {
+                                Console.WriteLine("Incorrect id input!");
+                            }
+                            catch (Exception er)
+                            {
+                                Console.WriteLine(er.InnerException?.Message);
+                            }
+                        }
+                        else
+                        {
+                            Console.WriteLine("Incorrect attribute name of element!");
+                        }
+                        
+                    }
+                    else if (option == "8")
                     {
                         goto Logging;
                     }
